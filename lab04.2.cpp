ef15846856f3903a2a19ae0bdf0112fede739116
unknown <olha.klishchevska.ri.2022@lpnu.ua.com>
2022-10-18 23:21:18 +0300

    first commit

---
+++ lab04.2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+    double x, xp, xk, dx, A, B, y;
+    cout << "xp = "; cin >> xp;
+    cout << "xk = "; cin >> xk;
+    cout << "dx = "; cin >> dx;
+    cout << fixed;
+    cout << "---------------------------" << endl;
+    cout << "|" << setw(5) << "x" << " |"
+        << setw(7) << "y" << " |" << endl;
+    cout << "---------------------------" << endl;
+    x = xp;
+    while (x <= xk)
+    {
+        A = 4.95 * x * x;
+        if (x < -3.5)
+            B = 4 + 1 / x * x;
+        else
+            if (x >= -3.5 && x < 1)
+                B = tan((3.5 + x) / 5.0);
+            else
+                if (x >= 1)
+                    B = sin(3 * x) - cos(x);
+        y = A + B;
+        cout << "|" << setw(7) << setprecision(2) << x
+            << " |" << setw(10) << setprecision(3) << y
+            << " |" << endl;
+        x += dx;
+    }
+    cout << "---------------------------" << endl;
+    return 0;
+}
+
